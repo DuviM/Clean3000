2bf20e11d1d5e24bf4f6f27e1a4649a7c0d1fbd7
Mathieu Duvillers <duvillers@gmail.com>
2023-09-05 17:23:54 +0200

    Avis de passage

--- src/views/Avis.vue
+++ src/views/Avis.vue
@@ -4,4 +4,5 @@
 
 <template>
   <h1 class="text-bold text-3xl text-gray-50 text-center my-10">AVIS DE PASSAGE</h1>
+  
 </template>
--- src/views/Formulaire.vue
+++ src/views/Formulaire.vue
@@ -1,6 +1,10 @@
 <script setup>
+  
   import AppButton from '../components/AppButton.vue';
   import AppSignaturePad from '../components/AppSignaturePad.vue';
+  
+
+
 </script>
 
 <template>
@@ -13,37 +17,37 @@
             client:
           </legend>
           <div>
-            <input type="checkbox" name="particulier" id="particulier">
+            <input type="checkbox" name="particulier" id="particulier" v-model="client" value="particulier">
             <label for="particulier" class="text-gray-50 ml-2">Particulier</label>
           </div>
           <div>
-            <input type="checkbox" name="Professionnel" id="particulier">
+            <input type="checkbox" name="professionnel" id="professionnel" v-model="client" value="professionnel">
             <label for="professionnel" class="text-gray-50 ml-2">Professionnel</label>
           </div>
         </fieldset>
         <div>
-          <input type="text" placeholder="Nom du client/Raison sociale" class="bg-gray-500 text-gray-50 rounded w-72 p-2 mt-2 md:mt-0" >
+          <input type="text" placeholder="Nom du client/Raison sociale" class="bg-gray-500 text-gray-50 rounded w-72 p-2 mt-2 md:mt-0" v-model="clientName">
         </div>
       </div>
       <div class="flex flex-col md:flex-row items-center justify-evenly mt-6">
         <label for="date" class="text-gray-50 ml-2">Date d'intervention :</label>
-        <input type="datetime-local" name="date" id="date" class="bg-gray-500 text-gray-50 rounded w-72 p-2 mt-2 md:mt-0">
+        <input type="datetime-local" name="date" id="date" class="bg-gray-500 text-gray-50 rounded w-72 p-2 mt-2 md:mt-0" v-model="dateIntervention">
       </div>
       <div class="flex flex-col md:flex-row items-center justify-evenly mt-6">
         <label for="observations" class="text-gray-50 ml-2">Observations :</label>
-        <textarea name="observations" id="observations" cols="30" rows="5" placeholder="Faites part de vos observations lors de l'intervention" class="bg-gray-500 text-gray-50 rounded px-2 w-72 mt-2 md:mt-0"></textarea>
+        <textarea name="observations" id="observations" cols="30" rows="5" placeholder="Faites part de vos observations lors de l'intervention" class="bg-gray-500 text-gray-50 rounded px-2 w-72 mt-2 md:mt-0" v-model="observations"></textarea>
       </div>
       <div class="flex flex-col md:flex-row items-center justify-evenly mt-6">
         <div class="flex flex-col items-center mt-0 md:mt-6">
           <label for="signatureTechnicien" class="text-gray-50 ml-2">Signature du technicien :</label>
-          <AppSignaturePad class="h-[150px] w-[300px]"></AppSignaturePad>
+          <AppSignaturePad class="h-[150px] w-[300px]" v-model="signatureTechnicien"></AppSignaturePad>
         </div>
         <div class="flex flex-col items-center mt-16 md:mt-6 mb-4 md:mb-0">
           <label for="signatureClient" class="text-gray-50 ml-2">Signature su client :</label>
-          <AppSignaturePad class="h-[150px] w-[300px]"></AppSignaturePad>
+          <AppSignaturePad class="h-[150px] w-[300px]" v-model="signatureClient"></AppSignaturePad>
         </div>
         <div>
-          <AppButton class="mt-16 md:mt-8">Valider l'avis de passage </AppButton>
+          <AppButton type="submit" class="mt-16 md:mt-8" @click="submitForm">Valider l'avis de passage</AppButton>
         </div>
       </div>
     </form>
